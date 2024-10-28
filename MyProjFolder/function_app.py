5c9a698415e9187d30726a0817a21a77357880dd
arct0r <109032203+arct0r@users.noreply.github.com>
2024-10-28 15:35:31 +0000

    sdsds

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -32,6 +32,7 @@ def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
              status_code=200
         )
 
+'''
 @app.route(route="PubChem", auth_level=func.AuthLevel.ANONYMOUS)
 def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
